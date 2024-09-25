ddad6300510c330925bbc0326c62df0877441722
Bmack1915 <119226403+Bmack1915@users.noreply.github.com>
2024-09-25 13:00:44 +0100

    Re-enter booking files to try to fix import errors from vercel

--- src/features/check-in-out/useTodayActivity.js
+++ src/features/check-in-out/useTodayActivity.js
@@ -1,12 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { getStaysTodayActivity } from "../../services/apiBookings";
 
-function useTodayActivity() {
+export function useTodayActivity() {
   const { isLoading, data: activities } = useQuery({
     queryFn: getStaysTodayActivity,
     queryKey: ["today-activity"],
   });
+
   return { activities, isLoading };
 }
-
-export default useTodayActivity;
