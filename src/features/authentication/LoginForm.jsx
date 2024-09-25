e704af0949c32372ebd661619e54ccca605fe37f
Bmack1915 <119226403+Bmack1915@users.noreply.github.com>
2024-09-25 13:27:19 +0100

    DefaultEmailAccount set up
    Login credentials provided so GitHub users may access site on fake account

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("nitavo9475@aiworldx.com");
+  const [password, setPassword] = useState("password123!");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
